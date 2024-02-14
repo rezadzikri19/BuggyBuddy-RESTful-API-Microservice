c2a6594999205f903ce269a1407de5942c0e16fc
uknow19 <rezadzikri@gmail.com>
2024-02-14 12:21:18 +0700

    fix formats

--- src/infrastructure/main_drivers/s3_revectorizer_driver.py
+++ src/infrastructure/main_drivers/s3_revectorizer_driver.py
@@ -16,10 +16,9 @@ class S3RevectorizerDriver(RevectorizerPort):
       bucket_name: str,
       logger: LoggerPort) -> None:
     session = boto3.Session(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=region_name
-      )
+      aws_access_key_id=aws_access_key_id,
+      aws_secret_access_key=aws_secret_access_key,
+      region_name=region_name)
     self.s3_client = session.client('s3')
     self.bucket_name = bucket_name
     self.logger = logger
